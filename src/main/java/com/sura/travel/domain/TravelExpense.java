a54cf4f037c8159d828f975b7b410f4f34bc9e28
Cristian Martinez <ccmartinez@sura.com.co>
2025-07-07 18:35:49 -0500

    Se creo la rama develop, tambien la carpeta domain con la logica requerida

---
+++ src/main/java/com/sura/travel/domain/TravelExpense.java
@@ -0,0 +1,23 @@
+package com.sura.travel.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class TravelExpense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String employeeName;
+    private LocalDate expenseDate;
+    private BigDecimal value;
+}
