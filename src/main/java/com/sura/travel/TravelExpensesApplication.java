6fd01d16524eae21625ae146d4f28adf57c7c757
Cristian Martinez <ccmartinez@sura.com.co>
2025-07-07 16:03:13 -0500

    Se hace la creación del repo de la API

---
+++ src/main/java/com/sura/travel/TravelExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.sura.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sura/travel/TravelExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sura.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
